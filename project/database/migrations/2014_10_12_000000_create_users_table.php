1c77aab3d88980fc27370e3864d536195497c626
olivenbarcelon <olivenbarcelon@gmail.com>
2023-09-13 15:13:20 +0800

    fix resources issue

--- project/database/migrations/2014_10_12_000000_create_users_table.php
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->uuid('uuid')->index();
-            $table->string('id_number')->index();
+            $table->string('id_number')->unique()->index();
             $table->string('email')->unique()->index();
             $table->string('password')->index();
             $table->string('mobile_number')->index();
@@ -25,7 +25,6 @@ class CreateUsersTable extends Migration {
             $table->timestamps();
             $table->softDeletes();
             $table->index(['created_at', 'updated_at', 'deleted_at']);
-            
         });
     }
 
